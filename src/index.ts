98ced890cad52311e7dc0f3a66c4c27c0c5d8624
Ons HAMAD <onshamadkl@gmail.com>
2024-07-23 11:51:18 +0100

    title

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export const configModuleName = "React Patterns Avancés" as const
+export const configModuleName = "React Hooks Avancés" as const
 export const iconUrl =
   "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" as const
